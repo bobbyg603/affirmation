06349665221bdd940d2100b98c29f0dbf6925081
Bobby Galli <bobbyg603@gmail.com>
2023-03-05 21:52:14 -0500

    feat: allow url override

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 
 export class AffirmationClient {
+    constructor(private url = 'https://www.affirmations.dev/') { }
+
     async get(): Promise<AffirmationResponse> {
-        const response = await fetch('https://www.affirmations.dev/');
+        const response = await fetch(this.url);
         const json = await response.json();
         return json as AffirmationResponse;
     }
